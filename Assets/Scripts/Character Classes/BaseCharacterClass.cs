1f991a6324f82af2e072d26039bd9f6e959de657
Mindleech <sven.eclipse91@gmail.com>
2016-01-05 01:38:17 +0100

    classes

--- Assets/Scripts/Character Classes/BaseCharacterClass.cs
+++ Assets/Scripts/Character Classes/BaseCharacterClass.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class BaseCharacterClass : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private string characterClassName;
+    private string characterClassDescription;
+    //Attributes
+    private int strength;
+    private int strength;
+    private int strength;
+    //Stats
+    
 }
